6605d503adece27daf03d8c7994ebf46e050435e
Brandon Mogren <mogren@hotmail.se>
2020-03-22 21:13:11 +0100

    INIT

---
+++ start.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(17, GPIO.OUT) #ENABLE MOTOR PIN
+GPIO.setup(27, GPIO.OUT) #IN1 FOR MOTOR 1
+
+while True:
+    GPIO.output(17, GPIO.HIGH)
+    GPIO.output(27, GPIO.HIGH)
