a3f1efba46d9459565c54b9eb7f0e9d1914e3d94
Brandon Mogren <mogren@hotmail.se>
2020-04-04 19:58:31 +0200

    asd

--- start.py
+++ start.py
@@ -6,13 +6,12 @@ GPIO.setwarnings(False)
 GPIO.setup(17, GPIO.OUT) #ENABLE MOTOR PIN
 GPIO.setup(27, GPIO.OUT) #IN1 FOR MOTOR 1
 
-GPIO.output(17, GPIO.HIGH)
-GPIO.output(27, GPIO.HIGH)
+#GPIO.output(17, GPIO.HIGH)
+#GPIO.output(27, GPIO.HIGH)
 
-GPIO.output(17, GPIO.LOW)
-GPIO.output(27, GPIO.LOW)
+#GPIO.output(17, GPIO.LOW)
+#GPIO.output(27, GPIO.LOW)
 
-
-#while True:
-    #GPIO.output(17, GPIO.HIGH)
-    #GPIO.output(27, GPIO.HIGH)
+while True:
+    GPIO.output(17, GPIO.HIGH)
+    GPIO.output(27, GPIO.HIGH)
