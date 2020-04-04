cd739cda39c7a7d47cca819246b094523269d934
Brandon Mogren <mogren@hotmail.se>
2020-04-04 20:08:50 +0200

    it should work now

--- start.py
+++ start.py
@@ -5,6 +5,7 @@ GPIO.setwarnings(False)
 
 GPIO.setup(17, GPIO.OUT) #ENABLE MOTOR PIN
 GPIO.setup(27, GPIO.OUT) #IN1 FOR MOTOR 1
+GPIO.setup(22, GPIO.OUT) #IN2 FOR MOTOR 2
 
 #GPIO.output(17, GPIO.HIGH)
 #GPIO.output(27, GPIO.HIGH)
@@ -13,5 +14,7 @@ GPIO.setup(27, GPIO.OUT) #IN1 FOR MOTOR 1
 #GPIO.output(27, GPIO.LOW)
 
 while True:
-    GPIO.output(17, GPIO.HIGH)
-    GPIO.output(27, GPIO.HIGH)
+    GPIO.output(17, True)
+    GPIO.output(27, True)
+    GPIO.output(22, False)
+    time.sleep(0.1)
