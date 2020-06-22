7d8b80e392210b12b73d5a1a6ac65a9ea88486f0
Brandon Mogren <mogren@hotmail.se>
2020-06-22 17:14:29 +0200

    Testing

--- start.py
+++ start.py
@@ -10,5 +10,6 @@ GPIO.setup(27, GPIO.OUT) #IN2 FOR MOTOR 2
 
 #GPIO.output(17, GPIO.LOW)
 #GPIO.output(27, GPIO.LOW)
-GPIO.output(17, True)
+while True:
+    GPIO.output(17, True)
 #GPIO.output(27, True)
