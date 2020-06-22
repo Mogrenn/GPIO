b694713e426e5175b5e0b9a8a97b5af82345179d
Brandon Mogren <mogren@hotmail.se>
2020-06-22 17:42:50 +0200

    Testing

--- start.py
+++ start.py
@@ -1,28 +1,84 @@
 import RPi.GPIO as GPIO
-import time
+from time import sleep
+
+in1 = 24
+in2 = 23
+en = 25
+temp1 = 1
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(en, GPIO.OUT)
+GPIO.output(in1, GPIO.LOW)
+GPIO.output(in2, GPIO.LOW)
+p = GPIO.PWM(en, 1000)
+
+p.start(25)
+print("\n")
+print("The default speed & direction of motor is LOW & Forward.....")
+print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
+print("\n")
+
+while (1):
+
+    x = input()
+
+    if x == 'r':
+        print("run")
+        if (temp1 == 1):
+            GPIO.output(in1, GPIO.HIGH)
+            GPIO.output(in2, GPIO.LOW)
+            print("forward")
+            x = 'z'
+        else:
+            GPIO.output(in1, GPIO.LOW)
+            GPIO.output(in2, GPIO.HIGH)
+            print("backward")
+            x = 'z'
 
 
-def init():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(17, GPIO.OUT)
-    GPIO.setup(27, GPIO.OUT)
+    elif x == 's':
+        print("stop")
+        GPIO.output(in1, GPIO.LOW)
+        GPIO.output(in2, GPIO.LOW)
+        x = 'z'
+
+    elif x == 'f':
+        print("forward")
+        GPIO.output(in1, GPIO.HIGH)
+        GPIO.output(in2, GPIO.LOW)
+        temp1 = 1
+        x = 'z'
+
+    elif x == 'b':
+        print("backward")
+        GPIO.output(in1, GPIO.LOW)
+        GPIO.output(in2, GPIO.HIGH)
+        temp1 = 0
+        x = 'z'
+
+    elif x == 'l':
+        print("low")
+        p.ChangeDutyCycle(25)
+        x = 'z'
+
+    elif x == 'm':
+        print("medium")
+        p.ChangeDutyCycle(50)
+        x = 'z'
+
+    elif x == 'h':
+        print("high")
+        p.ChangeDutyCycle(75)
+        x = 'z'
 
 
-def forward(tf):
-    init()
-    GPIO.output(17, True)
-    GPIO.output(27, False)
-    time.sleep(tf)
-    GPIO.cleanup()
+    elif x == 'e':
+        GPIO.cleanup()
+        print("GPIO Clean up")
+        break
 
-
-def reverse(tf):
-    init()
-    GPIO.output(17, False)
-    GPIO.output(27, True)
-    time.sleep(tf)
-    GPIO.cleanup()
-
-
-forward(4)
-reverse(2)
+    else:
+        print("<<<  wrong data  >>>")
+        print("please enter the defined data to continue.....")
