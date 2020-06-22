bd90e801ee09616ef73b3b9cba457037b0d3a8f1
Brandon Mogren <mogren@hotmail.se>
2020-06-22 16:23:46 +0200

    Added keyboard

--- start.py
+++ start.py
@@ -1,17 +1,47 @@
 import RPi.GPIO as GPIO
+from pynput.keyboard import Key, Listener
+
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+GPIO.setup(17, GPIO.OUT) #IN1 FOR MOTOR 1
+GPIO.setup(27, GPIO.OUT) #IN2 FOR MOTOR 2
 
-GPIO.setup(17, GPIO.OUT) #ENABLE MOTOR PIN
-GPIO.setup(27, GPIO.OUT) #IN1 FOR MOTOR 1
-GPIO.setup(22, GPIO.OUT) #IN2 FOR MOTOR 2
+def on_press(key):
+    print('{0} pressed'.format(
+        key))
+    try:
+        if key.char == 'w':
+            print("hje")
+            # GPIO.output(17, GPIO.HIGH)
+        elif key.char == "s":
+            # GPIO.output(27, GPIO.HIGH)
+    except Exception as e:
+        print(e)
+
+
+def on_release(key):
+    print('{0} release'.format(
+        key))
+    if key == Key.esc:
+        # Stop listener
+        return False
+    elif key.char == 'w':
+        GPIO.output(17, GPIO.HIGH)
+    elif key.char == "s":
+        GPIO.output(27, GPIO.HIGH)
+
+
+# Collect events until released
+with Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
 
 #GPIO.output(17, GPIO.HIGH)
 #GPIO.output(27, GPIO.HIGH)
 
 #GPIO.output(17, GPIO.LOW)
 #GPIO.output(27, GPIO.LOW)
-GPIO.output(17, True)
-GPIO.output(27, True)
-GPIO.output(22, True)
+#GPIO.output(17, True)
+#GPIO.output(27, True)
