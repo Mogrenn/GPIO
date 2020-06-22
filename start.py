46f0494f63f67400d4b1b98fab2ede3e1f7841b1
Brandon Mogren <mogren@hotmail.se>
2020-06-22 18:49:02 +0200

    Testing

--- start.py
+++ start.py
@@ -3,16 +3,15 @@ from time import sleep
 
 in1 = 17
 in2 = 27
-en = 25
-temp1 = 1
+servo = 22
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1, GPIO.OUT)
 GPIO.setup(in2, GPIO.OUT)
-GPIO.setup(en, GPIO.OUT)
+GPIO.setup(servo, GPIO.OUT)
 GPIO.output(in1, GPIO.LOW)
 GPIO.output(in2, GPIO.LOW)
-p = GPIO.PWM(en, 1000)
+p = GPIO.PWM(servo, 50)
 
 p.start(25)
 print("\n")
@@ -36,7 +35,6 @@ while (1):
             GPIO.output(in2, GPIO.HIGH)
             print("backward")
             x = 'z'
-
 
     elif x == 's':
         print("stop")
@@ -73,6 +71,10 @@ while (1):
         p.ChangeDutyCycle(75)
         x = 'z'
 
+    elif x == 'l':
+        print("left")
+        p.ChangeDutyCycle(5)
+        x = 'z'
 
     elif x == 'e':
         GPIO.cleanup()
