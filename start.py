e56f14d1183dce4035b266fdfd2235ee5c053877
Brandon Mogren <mogren@hotmail.se>
2020-06-22 17:13:51 +0200

    Testing

--- start.py
+++ start.py
@@ -5,10 +5,10 @@ GPIO.setwarnings(False)
 GPIO.setup(17, GPIO.OUT) #IN1 FOR MOTOR 1
 GPIO.setup(27, GPIO.OUT) #IN2 FOR MOTOR 2
 
-GPIO.output(17, GPIO.HIGH)
+#GPIO.output(17, GPIO.HIGH)
 #GPIO.output(27, GPIO.HIGH)
 
 #GPIO.output(17, GPIO.LOW)
 #GPIO.output(27, GPIO.LOW)
-#GPIO.output(17, True)
+GPIO.output(17, True)
 #GPIO.output(27, True)
