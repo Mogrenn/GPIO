247c170bebd1a29f11e9f3f3f2ae3b48a5c9d919
Brandon Mogren <mogren@hotmail.se>
2020-06-22 17:33:00 +0200

    Testing

--- start.py
+++ start.py
@@ -1,16 +1,28 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(17, GPIO.OUT) #IN1 FOR MOTOR 1
-GPIO.setup(27, GPIO.OUT) #IN2 FOR MOTOR 2
 
-#GPIO.output(17, GPIO.HIGH)
-#GPIO.output(27, GPIO.HIGH)
 
-#GPIO.output(17, GPIO.LOW)
-#GPIO.output(27, GPIO.LOW)
-while True:
+def init():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(17, GPIO.OUT)
+    GPIO.setup(27, GPIO.OUT)
+
+
+def forward(tf):
+    init()
+    GPIO.output(17, True)
+    GPIO.output(27, False)
+    time.sleep(tf)
+    GPIO.cleanup()
+
+
+def reverse(tf):
+    init()
     GPIO.output(17, False)
     GPIO.output(27, True)
-#GPIO.output(27, True)
+    time.sleep(tf)
+    GPIO.cleanup()
+
+
+forward(4)
+reverse(2)
